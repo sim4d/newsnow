9cb3e865c6cee364d172c1eb89c0ae435bb48f6f
Simford.Dong <sim4dong@gmail.com>
2026-04-07 23:38:00 +0800

    fix: parse RSS feeds using text response

--- server/utils/rss2json.ts
+++ server/utils/rss2json.ts
@@ -4,7 +4,9 @@ import type { RSSInfo } from "../types"
 export async function rss2json(url: string): Promise<RSSInfo | undefined> {
   if (!/^https?:\/\/[^\s$.?#].\S*/i.test(url)) return
 
-  const data = await myFetch(url)
+  const data = await myFetch<string>(url, {
+    responseType: "text",
+  })
 
   const xml = new XMLParser({
     attributeNamePrefix: "",
@@ -12,7 +14,7 @@ export async function rss2json(url: string): Promise<RSSInfo | undefined> {
     ignoreAttributes: false,
   })
 
-  const result = xml.parse(data as string)
+  const result = xml.parse(data)
 
   let channel = result.rss && result.rss.channel ? result.rss.channel : result.feed
   if (Array.isArray(channel)) channel = channel[0]
